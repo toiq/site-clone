df4e68efc29939cb959cf48aaa95d579eb27713d
toiq <tousif.iqbal@outlook.com>
2025-02-19 01:15:53 +0600

    Responsive menu

--- src/features/landing/navbar/navbar.tsx
+++ src/features/landing/navbar/navbar.tsx
@@ -28,6 +28,7 @@ export const Navbar = () => {
   const { scrollY } = useScroll();
   const [lastScrollY, setLastScrollY] = useState(0);
   const [isVisible, setIsVisible] = useState(true);
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
 
   useEffect(() => {
     return scrollY.onChange((current) => {
@@ -40,6 +41,18 @@ export const Navbar = () => {
     });
   }, [scrollY, lastScrollY, isVisible]);
 
+  useEffect(() => {
+    if (isMobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isMobileMenu]);
+
   return (
     <motion.nav
       initial={{ y: 0 }}
@@ -48,13 +61,13 @@ export const Navbar = () => {
       className="sticky top-0 w-full border border-transparent border-b-[#e8eef7] bg-white/50 py-[21px] font-manrope backdrop-blur-[48px]"
     >
       <div className="container max-w-screen-xl">
-        <div className="flex items-center justify-between px-4">
+        <div className="flex items-center justify-between px-6 md:px-12 lg:px-4">
           <Link href="/">
             <Image src="/logo.png" width={44} height={44} alt="Logo" />
           </Link>
 
           <div className="flex items-center gap-x-10">
-            <ul className="-mt-0.5 flex items-center justify-between gap-x-10">
+            <ul className="-mt-0.5 hidden items-center justify-between gap-x-10 lg:flex">
               {links.map((item, i) => (
                 <li key={i}>
                   <Link
@@ -67,14 +80,44 @@ export const Navbar = () => {
               ))}
             </ul>
 
-            <Button asChild size="small">
-              <Link href="/contact-us" className="h-full w-full">
-                Contact Us
-              </Link>
-            </Button>
+            {!isMobileMenu && (
+              <Button asChild size="small" className="self-end">
+                <Link href="/contact-us" className="h-full w-full">
+                  Contact Us
+                </Link>
+              </Button>
+            )}
+
+            <Image
+              src={"https://www.dipainhouse.com/assets/svg/ic-bars.svg"}
+              width={24}
+              height={24}
+              alt="Menu"
+              className="py-[14px]"
+              onClick={() => setIsMobileMenu((prev) => !prev)}
+            />
           </div>
         </div>
       </div>
+      {isMobileMenu && (
+        <div className="z-[99] h-screen w-full bg-white text-center">
+          <ul className="mt-6 flex h-full flex-col items-center">
+            {links.map((item, i) => (
+              <li key={i} className="mb-5">
+                <Link
+                  href={item.link}
+                  className="min-w-5 text-[22px] font-bold text-primary hover:text-accent"
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+            <Button asChild size="small" className="mt-10">
+              <Link href="/contact-us">Contact Us</Link>
+            </Button>
+          </ul>
+        </div>
+      )}
     </motion.nav>
   );
 };
